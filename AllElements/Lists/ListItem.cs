31aad941ad5aa1c160d1232ff0cc27d81c275ec1
redmoongames <redmoon.thebest@gmail.com>
2022-11-12 17:05:17 +0700

    Refactor

--- AllElements/Lists/ListItem.cs
+++
@@ -1,9 +0,0 @@
-namespace ElementsLib.AllElements;
-
-public class ListItem
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Price { get; set; }
-    public string ImageSource { get; set; }
-}
